4db828b2c726f0bc43b8f8650220ffca418fc824
tsucchiiinoko <ccya2211@icloud.com>
2021-11-22 15:35:05 +0900

    add Model; Comment, Like and Retweet

---
+++ ChatLikeSampler/Model/Comment.swift
@@ -0,0 +1,29 @@
+//
+//  Comment.swift
+//  ChatLikeSampler
+//
+//  Created by Daichi Tsuchiya on 2021/11/22.
+//
+
+import Foundation
+import Firebase
+
+class Comment {
+    let profile_icon: String
+    let email: String
+    let username: String
+    let text: String
+    let is_deleted: Bool
+    let created_at: Timestamp
+    let updated_at: Timestamp
+    
+    init(data: [String: Any]) {
+        self.profile_icon = data["profile_icon"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.username = data["username"] as? String ?? ""
+        self.text = data["text"] as? String ?? ""
+        self.is_deleted = data["is_deleted"] as? Bool ?? false
+        self.created_at = data["created_at"] as? Timestamp ?? Timestamp()
+        self.updated_at = data["updated_at"] as? Timestamp ?? Timestamp()
+    }
+}
---
+++ ChatLikeSampler/Model/Like.swift
@@ -0,0 +1,27 @@
+//
+//  Like.swift
+//  ChatLikeSampler
+//
+//  Created by Daichi Tsuchiya on 2021/11/22.
+//
+
+import Foundation
+import Firebase
+
+class Like {
+    let profile_icon: String
+    let email: String
+    let username: String
+    let abount: String
+    let created_at: Timestamp
+    let updated_at: Timestamp
+    
+    init(data: [String: Any]) {
+        self.profile_icon = data["profile_icon"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.username = data["username"] as? String ?? ""
+        self.abount = data["abount"] as? String ?? ""
+        self.created_at = data["created_at"] as? Timestamp ?? Timestamp()
+        self.updated_at = data["updated_at"] as? Timestamp ?? Timestamp()
+    }
+}
