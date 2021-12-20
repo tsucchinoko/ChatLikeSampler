1f5125122b9146fe12639b1c68fa90abb59dbdcd
tsucchiiinoko <ccya2211@icloud.com>
2021-12-21 06:46:04 +0900

    タグによる検索を部分一致になるよう修正

--- ChatLikeSampler/View/SearchUser/SearchUserViewController.swift
+++ ChatLikeSampler/View/SearchUser/SearchUserViewController.swift
@@ -23,9 +23,17 @@ class SearchUserViewController: UIViewController {
     
     @IBOutlet weak var userListTableView: UITableView!
     
+    @IBOutlet weak var desiredIndustryPicker: UIPickerView!
     
+    @IBOutlet weak var desiredOccupationPicker: UIPickerView!
     
+    @IBOutlet weak var desiredAreaPicker: UIPickerView!
     
+    var dataList = [[String]]()
+    var combination = [String]()
+    var selectedIndustry = "未選択"
+    var selectedOccupation = "未選択"
+    var selectedArea = "未選択"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,17 +52,73 @@ class SearchUserViewController: UIViewController {
         userListTableView.delegate = self
         userListTableView.dataSource = self
         
+        for _ in 0 ... 2 {
+            dataList.append([])
+        }
+        
+        desiredIndustryPicker.tag = 0
+        desiredOccupationPicker.tag = 1
+        desiredAreaPicker.tag = 2
+        
+        dataList[0] = ["未選択", "IT","広告","金融","保険"]
+        dataList[1] = ["未選択", "エンジニア","ITコンサル","銀行員", "FP"]
+        dataList[2] = ["未選択", "関東","東北","関西"]
+        
+        combination = ["未選択", "未選択", "未選択"]
+        
+        
+        desiredIndustryPicker.delegate = self
+        desiredIndustryPicker.dataSource =  self
+        desiredOccupationPicker.delegate = self
+        desiredOccupationPicker.dataSource = self
+        desiredAreaPicker.delegate = self
+        desiredAreaPicker.dataSource = self
+        
     }
     
-    private func fetchUserInfoFromFirestore(text: String){
-        db.collection("users").order(by: "username").start(at: [text]).end(at: [text + "\u{f8ff}"]).getDocuments { userSnapshots, err in
+    // ユーザー検索
+    private func fetchUserInfoFromFirestore(text: String, industry: String, occupation: String, area: String){
+        self.users.removeAll()
+//        var userList = []
+        db.collection("users").order(by: "username").start(at: [text]).end(at: [text + "\u{f8ff}"]).whereField("desired_industry", arrayContainsAny: ["未選択", industry]).getDocuments { userSnapshots, err in
             if let err = err {
                 print("ユーザー情報の取得に失敗しました: \(err)")
                 return
             }
             
-            if text == "" { return }
-            self.users.removeAll()
+            if text == "" && industry == "未選択" { return }
+            // Tweet情報を配列に格納
+            guard let snapshots = userSnapshots?.documents else { return }
+            self.users = snapshots.map { document in
+                let user = User(document: document)
+                print("##user!!: \(user)")
+                return user
+            }
+        }
+        
+        db.collection("users").order(by: "username").start(at: [text]).end(at: [text + "\u{f8ff}"]).whereField("desired_occupation", arrayContainsAny: ["未選択", occupation]).getDocuments { userSnapshots, err in
+            if let err = err {
+                print("ユーザー情報の取得に失敗しました: \(err)")
+                return
+            }
+            
+            if text == "" && occupation == "未選択" { return }
+            // Tweet情報を配列に格納
+            guard let snapshots = userSnapshots?.documents else { return }
+            self.users = snapshots.map { document in
+                let user = User(document: document)
+                print("##user!!: \(user)")
+                return user
+            }
+        }
+        
+        db.collection("users").order(by: "username").start(at: [text]).end(at: [text + "\u{f8ff}"]).whereField("desired_area", arrayContainsAny: ["未選択", area]).getDocuments { userSnapshots, err in
+            if let err = err {
+                print("ユーザー情報の取得に失敗しました: \(err)")
+                return
+            }
+            
+            if text == "" && area == "未選択" { return }
             // Tweet情報を配列に格納
             guard let snapshots = userSnapshots?.documents else { return }
             self.users = snapshots.map { document in
@@ -65,7 +129,7 @@ class SearchUserViewController: UIViewController {
         }
     }
     
-
+    
     @IBAction func didTappedPreferenceButton(_ sender: Any) {
     }
     
@@ -76,12 +140,56 @@ class SearchUserViewController: UIViewController {
             self.userListTableView.reloadData()
         }
     }
+}
+
+extension SearchUserViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewの行数、要素の全数
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        print("dataList.count: \(dataList.count)")
+        return dataList[pickerView.tag].count
+    }
+    
+    // UIPickerViewに表示する配列
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        return dataList[pickerView.tag][row]
+    }
+    
+    // UIPickerViewのRowが選択された時の挙動
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        combination[pickerView.tag] = dataList[pickerView.tag][row]
+        selectedIndustry = combination[0]
+        selectedOccupation = combination[1]
+        selectedArea = combination[2]
+        print("combination: \(combination)")
+    }
+    
+    // picker内のテキストサイズを調整
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = (view as? UILabel) ?? UILabel()
+        label.text = self.dataList[pickerView.tag][row]
+        label.textAlignment = .center
+        // テキストが長い場合に見きれないようにする
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }
+    
     
 }
 
 extension SearchUserViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        fetchUserInfoFromFirestore(text: searchText)
+        fetchUserInfoFromFirestore(text: searchText, industry: selectedIndustry, occupation: selectedOccupation, area: selectedArea)
         refreshTableView()
     }
 }
@@ -90,9 +198,9 @@ extension SearchUserViewController: UITableViewDelegate, UITableViewDataSource {
     
     // セルの高さ
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        userListTableView.estimatedRowHeight = 100
+        //        userListTableView.estimatedRowHeight = 100
         // テキストビューの高さによってセルの高さを自動で変化させる
-//        return UITableView.automaticDimension
+        //        return UITableView.automaticDimension
         return 80
     }
     // セルの数
@@ -118,7 +226,7 @@ extension SearchUserViewController: UITableViewDelegate, UITableViewDataSource {
 
 
 class UserListTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var userImageView: UIImageView!
     
     @IBOutlet weak var userNameLabel: UILabel!
@@ -132,12 +240,12 @@ class UserListTableViewCell: UITableViewCell {
     var user: User? {
         didSet {
             if let user = user {
-//                TODO String -> UIImage
-//                userImageView.image = UIImage(user.profile_icon)
+                //                TODO String -> UIImage
+                //                userImageView.image = UIImage(user.profile_icon)
                 userNameLabel.text = user.username
                 userAboutTextView.text = user.about
-
-//                dateLabel.text = dateFormatterForDateLabel(date: room.latestMessage?.created_at.dateValue() ?? Date())
+                
+                //                dateLabel.text = dateFormatterForDateLabel(date: room.latestMessage?.created_at.dateValue() ?? Date())
             }
         }
     }
